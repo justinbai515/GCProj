e41ef2bb3d52fa6f8d533334c2d6aa8a81af2804
justinbai515 <74213057+justinbai515@users.noreply.github.com>
2021-08-24 19:25:31 -0500

    Delete keys.py

--- keys.py
+++
@@ -1,3 +0,0 @@
-def getKeys(num):
-    keyList = ["eWB8NKIgWcxMZGiOvbs6TXWPO", "ntF53E31THMeedlNIMYToX7Kstl8ezpKKQiI7NaBS9ixUOKXdK", "4321234575-dsRBHEe13B5HBNv0AOZR9ImbxRa07pariEfSP2E", "RyZqQrsRVWTP5TtyLnBrrhYzcIzepKmt7AGXTEY1CqiMm"]
-    return (keyList[num])
