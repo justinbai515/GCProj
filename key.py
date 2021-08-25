750f2624428e30a8f3feb7881a13135dae9e40d4
justinbai515 <74213057+justinbai515@users.noreply.github.com>
2021-08-24 19:23:44 -0500

    Create key.py

---
+++ key.py
@@ -0,0 +1,3 @@
+def getKeys(num):
+    keyList = ["eWB8NKIgWcxMZGiOvbs6TXWPO", "ntF53E31THMeedlNIMYToX7Kstl8ezpKKQiI7NaBS9ixUOKXdK", "4321234575-dsRBHEe13B5HBNv0AOZR9ImbxRa07pariEfSP2E", "RyZqQrsRVWTP5TtyLnBrrhYzcIzepKmt7AGXTEY1CqiMm"]
+    return (keyList[num])
